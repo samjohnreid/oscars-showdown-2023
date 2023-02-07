27b0cecdfca8ef89f86cfef4980c37efe96e8901
samjohnreid <samjohnreid@gmail.com>
2023-02-07 15:34:37 -0500

    setting up Sanity CMS

---
+++ oscars-showdown-2023-cms/oscars-showdown-2023/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Oscars Showdown 2023',
+
+  projectId: 'd2cpdrm6',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
