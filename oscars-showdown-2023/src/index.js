47c6f8fea202ddc064161e4ce4b4d42d112fff8f
samjohnreid <samjohnreid@gmail.com>
2023-02-17 17:10:29 -0500

    sorting through data from Content Lake

--- oscars-showdown-2023/src/index.js
+++ oscars-showdown-2023/src/index.js
@@ -6,40 +6,97 @@ const PROJECT_ID = "d2cpdrm6";
 const DATASET = "production";
 const API_PATH = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=`;
 
-const QUERY_PETS = encodeURIComponent('*[_type == "pet"]');
+const QUERY_PLAYER = encodeURIComponent('*[_type == "player"]');
+const QUERY_DIRECTOR = encodeURIComponent('*[_type == "director"]');
+const QUERY_PICTURE = encodeURIComponent('*[_type == "picture"]');
 
-const petsPromise = fetch(`${API_PATH}${QUERY_PETS}`);
+const playerPromise = fetch(`${API_PATH}${QUERY_PLAYER}`);
+const directorPromise = fetch(`${API_PATH}${QUERY_DIRECTOR}`);
+const picturePromise = fetch(`${API_PATH}${QUERY_PICTURE}`);
 
-function SelectBestPicture(hello) {
-    console.log('hi sab 👋', hello);
+const playerName = 'Dom';
+
+const PlayerNoms = (props) => {
+    console.log('props:', props);
+    console.log('player:', props.player);
+    console.log('director:', props.director);
+    console.log('picture:', props.picture);
+
+    const nomOptions = (noms) => {
+        return noms.map((nom, index) => (
+            <option value={nom.title ? nom.title : nom.name} key={index}>{nom.title ? nom.title : nom.name}</option>
+        ));
+    }
+
+    const findCurrentNom = (cat) => {
+        // 1. find out *who* the player is ✅
+        // 2. find out what category is ✅
+        // 3. find out if they voted yet, and if so what the nom is
+        // 4. default to the first one if no nom; add selected to nom if one
+        console.log('hellooooo!', cat);
+
+        const playerData = props.player.find((el) => {
+            return el.name.includes(playerName);
+        });
+
+        console.log('hi sab', playerData);
+    }
+    
+    console.log('props.player', props.player);
+    
     return (
         <div>
-            <label htmlFor="bestPicture">Please select Best Picture:</label>
-            <select id="bestPicture">
-                <option>Hello!</option>
-                <option>Goodbye!</option>
-            </select>
+            <h2>Nominations for [player]!</h2>
+            <br />
+            <form>
+                <div>
+                    <label>Best Picture: </label>
+                    <select defaultValue={findCurrentNom('Best Picture')}>
+                        {nomOptions(props.picture)}
+                    </select>
+                </div>
+                <div>
+                    <label>Best Director: </label>
+                    <select>
+                        {nomOptions(props.director)}
+                    </select>
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </form>
         </div>
     );
 }
 
 function App() {
-    const [pets, setPets] = useState(null);
+    const [player, setPlayer] = useState(null);
+    const [director, setDirector] = useState(null);
+    const [picture, setPicture] = useState(null);
+    const [dataFetched, setDataFetched] = useState(false);
 
     useEffect(() => {
         Promise
-            .all([petsPromise])
+            .all([
+                playerPromise,
+                directorPromise,
+                picturePromise
+            ])
             .then((responses) => {
                 return Promise.all(responses.map((response) => {
                     return response.json();
                 }))
             })
             .then((data) => {
-                console.log('data:', data[0].result);
-                setPets(data[0].result);
+                setPlayer(data.find(el => el.query.includes('"player"]')).result);
+                setDirector(data.find(el => el.query.includes('"director"]')).result);
+                setPicture(data.find(el => el.query.includes('"picture"]')).result);
+            })
+            .then(() => {
+                setDataFetched(true);
             })
             .catch((error) => {
-                console.error('Error!', error);
+                console.error('Error! Something went wrong 😬', error);
             })
     }, []);
     
@@ -47,7 +104,8 @@ function App() {
         <div>
             <h1>Hello!</h1>
             <p>Welcome to Oscars Showdown 2023!</p>
-            {pets && <SelectBestPicture hello={pets} />}
+            <hr style={{margin: '50px 0'}} />
+            {dataFetched && <PlayerNoms player={player} director={director} picture={picture} />}
         </div>
     );
 }
