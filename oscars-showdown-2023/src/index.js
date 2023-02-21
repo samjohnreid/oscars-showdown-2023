013e547886b6829775f6cc5a4b85caeca757779d
samjohnreid <samjohnreid@gmail.com>
2023-02-21 13:06:31 -0500

    updating Sanity CMS schema

--- oscars-showdown-2023/src/index.js
+++ oscars-showdown-2023/src/index.js
@@ -9,28 +9,31 @@ const API_PATH = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${D
 const QUERY_PLAYER = encodeURIComponent('*[_type == "player"]');
 const QUERY_DIRECTOR = encodeURIComponent('*[_type == "director"]');
 const QUERY_PICTURE = encodeURIComponent('*[_type == "picture"]');
+const QUERY_ACTOR = encodeURIComponent('*[_type == "actor"]');
 
 const playerPromise = fetch(`${API_PATH}${QUERY_PLAYER}`);
 const directorPromise = fetch(`${API_PATH}${QUERY_DIRECTOR}`);
 const picturePromise = fetch(`${API_PATH}${QUERY_PICTURE}`);
+const actorPromise = fetch(`${API_PATH}${QUERY_ACTOR}`);
 
-const playerName = 'Dom';
+const playerName = 'Sam';
 
 const PlayerNoms = (props) => {
     const nomOptions = (noms) => {
-        return noms.map((nom, index) => (
+        const sortedNoms = noms.sort((a, b) => {
+            const nomA = a.name ? a.name.toUpperCase() : a.title.toUpperCase();
+            const nomB = b.name ? b.name.toUpperCase() : b.title.toUpperCase();
+            return nomA < nomB ? -1 : 1;
+        });
+
+        return sortedNoms.map((nom, index) => (
             <option value={nom.title ? nom.title : nom.name} key={index}>{nom.title ? nom.title : nom.name}</option>
         ));
     }
 
-    const findCurrentNom = (catArr, catName) => {
-        // 1. find out *who* the player is ✅
-        // 2. find out what category is ✅
-        // 3. find out if they voted yet, and if so what the nom is
-        // 4. default to the first one if no nom; add selected to nom if one
-        
+    const findCurrentNom = (category) => {
         const playerData = props.player.find(el => el.name.includes(playerName));
-        const playerNom = catArr.find(item => item._id === playerData[catName]._ref);
+        const playerNom = category.find(item => item._id === playerData[category[0]._type]._ref);
 
         return playerNom.title ? playerNom.title : playerNom.name;
     }
@@ -42,14 +45,20 @@ const PlayerNoms = (props) => {
             <form>
                 <div>
                     <label>Best Picture: </label>
-                    <select defaultValue={findCurrentNom(props.picture, 'picture')}>
+                    <select defaultValue={findCurrentNom(props.picture)}>
                         {nomOptions(props.picture)}
                     </select>
                 </div>
                 <div>
                     <label>Best Director: </label>
-                    <select defaultValue={findCurrentNom(props.director, 'director')}>
+                    <select defaultValue={findCurrentNom(props.director)}>
                         {nomOptions(props.director)}
+                    </select>
+                </div>
+                <div>
+                    <label>Best Actor: </label>
+                    <select defaultValue={findCurrentNom(props.actor)}>
+                        {nomOptions(props.actor)}
                     </select>
                 </div>
                 <div>
@@ -64,6 +73,7 @@ function App() {
     const [player, setPlayer] = useState(null);
     const [director, setDirector] = useState(null);
     const [picture, setPicture] = useState(null);
+    const [actor, setActor] = useState(null);
     const [dataFetched, setDataFetched] = useState(false);
 
     useEffect(() => {
@@ -71,7 +81,8 @@ function App() {
             .all([
                 playerPromise,
                 directorPromise,
-                picturePromise
+                picturePromise,
+                actorPromise
             ])
             .then((responses) => {
                 return Promise.all(responses.map((response) => {
@@ -82,6 +93,7 @@ function App() {
                 setPlayer(data.find(el => el.query.includes('"player"]')).result);
                 setDirector(data.find(el => el.query.includes('"director"]')).result);
                 setPicture(data.find(el => el.query.includes('"picture"]')).result);
+                setActor(data.find(el => el.query.includes('"actor"]')).result);
             })
             .then(() => {
                 setDataFetched(true);
@@ -96,7 +108,12 @@ function App() {
             <h1>Hello!</h1>
             <p>Welcome to Oscars Showdown 2023!</p>
             <hr style={{margin: '50px 0'}} />
-            {dataFetched && <PlayerNoms player={player} director={director} picture={picture} />}
+            {dataFetched && <PlayerNoms
+                player={player}
+                director={director}
+                picture={picture}
+                actor={actor}
+            />}
         </div>
     );
 }
