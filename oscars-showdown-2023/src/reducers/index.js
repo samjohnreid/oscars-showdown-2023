c9d6907c4477293b13513da4c9399ed3e02bc3ba
samjohnreid <samjohnreid@gmail.com>
2024-06-12 22:20:20 -0400

    adding combineReducers for future reducers

--- oscars-showdown-2023/src/reducers/index.js
+++ oscars-showdown-2023/src/reducers/index.js
@@ -1,17 +1,26 @@
 // src/reducers/index.js
+import { combineReducers } from 'redux';
+
+// Reducers
 const initialState = {
-    counter: 0
-  };
-  
-  const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "INCREMENT":
-        return { ...state, counter: state.counter + 1 };
-      case "DECREMENT":
-        return { ...state, counter: state.counter - 1 };
-      default:
-        return state;
-    }
-  };
-  
-  export default counterReducer;
+  counter: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, counter: state.counter + 1 };
+    case 'DECREMENT':
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
+
+// Combine Reducers
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  // Other reducers here...
+});
+
+export default rootReducer;
